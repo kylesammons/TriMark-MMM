53e1d1e866232dac3850a7ae1d4913e7ebcc28c6
kylesammons <141177424+kylesammons@users.noreply.github.com>
2024-11-27 10:03:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 # Set Streamlit page config
-st.set_page_config(page_title="The Reef", page_icon=":male_mage::skin-tone-4:")
+st.set_page_config(page_title="The Reef", page_icon=":male_mage:")
 
 # App title
-st.title(":male_mage::skin-tone-4: The Reef")
+st.title(":male_mage: TriMark MMM")
