9249b9dab8a0ed6e30fd06533e7809b4142d5948
kylesammons <141177424+kylesammons@users.noreply.github.com>
2024-11-27 20:36:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,8 +105,8 @@ if data_files:
     # Store the selected response variable if the form is submitted
     if submit_button:
             with  st.spinner("Training the MMM model..."):
-            st.session_state["response_variable"] = response_var
-            st.success(f"Response variable {response_var} selected!")
+                st.session_state["response_variable"] = response_var
+                st.success(f"Response variable {response_var} selected!")
 
 else:
     st.error("No columns found in the uploaded file.")
